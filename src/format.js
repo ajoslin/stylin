c5a006f176140fec61bcc15294282efb90343a2a
Andrew Joslin <andrewtjoslin@gmail.com>
2016-04-08 18:35:02 -0700

    Use camel-2-dash, not param-case

--- src/format.js
+++ src/format.js
@@ -1,5 +1,5 @@
 var Prefixer = require('inline-style-prefixer')
-var paramCase = require('param-case')
+var dashCase = require('camel-2-dash')
 
 var prefixer = new Prefixer({keepUnprefixed: true})
 
@@ -13,7 +13,7 @@ module.exports = function formatStyles (selector, style) {
       result[selector + key] = style[key]
     } else {
       // Copy normal styles into the normal selector
-      result[selector][paramCase(key)] = style[key]
+      result[selector][dashCase(key)] = style[key]
     }
   })
 
