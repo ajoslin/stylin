f2d852abecb450e0ee5a67b480fcc5cc58e4fd0b
Andrew Joslin <andrewtjoslin@gmail.com>
2016-05-02 15:07:44 -0700

    Throw error if passed in value is not an object

--- src/index.js
+++ src/index.js
@@ -7,16 +7,22 @@ css.rule = rule
 css.keyframes = keyframes
 css.getCss = getCss
 
-function css (rules) {
-  return freeStyle().registerStyle(prefix(rules))
+function css (style) {
+  if (!style) throw new TypeError('css style object expected')
+
+  return freeStyle().registerStyle(prefix(style))
 }
 
-function rule (key, rules) {
-  return freeStyle().registerRule(key, prefix(rules))
+function rule (key, style) {
+  if (!style) throw new TypeError('css style object expected')
+
+  return freeStyle().registerRule(key, prefix(style))
 }
 
-function keyframes (rules) {
-  return freeStyle().registerKeyframes(prefix(rules))
+function keyframes (style) {
+  if (!style) throw new TypeError('css style object expected')
+
+  return freeStyle().registerKeyframes(prefix(style))
 }
 
 function getCss () {
--- test.js
+++ test.js
@@ -1,10 +1,24 @@
 var test = require('tape')
-var css = require('./')
+var stylin = require('./')
 
 test(function (t) {
-  var className = css({color: 'red'})
+  var className = stylin({color: 'red'})
   t.equal(typeof className, 'string')
 
-  t.equal(css.getCss(), '.' + className + '{color:red}')
+  t.equal(stylin.getCss(), '.' + className + '{color:red}')
+  t.end()
+})
+
+test('errors', function (t) {
+  t.throws(function () {
+    stylin.css()
+  }, TypeError)
+  t.throws(function () {
+    stylin.rule('foo')
+  }, TypeError)
+  t.throws(function () {
+    stylin.keyframes()
+  }, TypeError)
+
   t.end()
 })
